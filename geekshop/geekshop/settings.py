b62c0dcd8317311f5fd3626797a3fe95b677be23
ChausovDS <61182311+ChausovDS@users.noreply.github.com>
2020-10-15 13:04:31 +0300

    Lesson 3 (#4)
    * Lesson-2. templates and context
    
    * Lesson 3. Django ORM start
    
    * Lesson 3. Djagno admin
    
    * Lesson3 DB and ORM
    
    * Lesson3 DB and ORM

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'pzegz3ade%pvc00#@7bxcz9mt8tv(^l!5a9iks0t_@)ra#&om$'
+SECRET_KEY = 'qh(e3v$4f(8rjx#5krh#a#8&ed8b3#s$_&@c=z-8j1r3*k!nz*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -121,11 +121,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-
-    os.path.join(BASE_DIR, "static"),
+os.path.join(BASE_DIR, "static"),
 )
-
-MEDIA_URL = '/media/'
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -1,5 +1,3 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
@@ -16,7 +14,3 @@ urlpatterns = [
     path("products/classic", mainapp.products, name="products_classic"),
     path("contact/", mainapp.contact, name="contact"),
 ]
-
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
